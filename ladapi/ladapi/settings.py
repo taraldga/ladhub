02160efb39b29df98670b3c8bd89434b4d1fbf4f
Tarald <taraldga@gmail.com>
2022-05-14 08:58:55 +0200

    feat: add drf and jwt to backend

--- ladapi/ladapi/settings.py
+++ ladapi/ladapi/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party apps
+    'rest_framework',
+    'rest_framework_simplejwt',
+
+    # My apps
 ]
 
 MIDDLEWARE = [
@@ -122,3 +127,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
