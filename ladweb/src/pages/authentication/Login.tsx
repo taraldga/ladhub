202448942f7ce487dddae7eea34ca5ba2177a9fa
taraldgasbakk <taraldga@gmail.com>
2022-05-28 10:20:28 +0200

    feat: individual error messages and form labels

--- ladweb/src/pages/authentication/Login.tsx
+++ ladweb/src/pages/authentication/Login.tsx
@@ -1,37 +1,60 @@
-import React from 'react'
-import { Button, Container, FormControl, FormErrorMessage, FormLabel, Heading, Input, VStack } from '@chakra-ui/react'
-import { useForm } from 'react-hook-form';
+import React from "react";
+import {
+  Button,
+  Container,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  Input,
+  VStack,
+} from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
 
 function Login() {
-
   const {
     handleSubmit,
     register,
     formState: { errors, isSubmitting },
-  } = useForm()
+  } = useForm();
 
-  const onSubmit = () => {
+  const onSubmit = () => {};
 
-  }
-  
   return (
-    <VStack spacing={'10px'}>
+    <VStack spacing={"10px"}>
       <Heading>Log in</Heading>
       <Container>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <FormControl isInvalid={errors.username || errors.password}>
-            <FormLabel htmlFor='username'>Username</FormLabel>
-            <Input id='username' placeholder='Username' {...register('username', {
-              required: 'Username is required'
-            })} />
-            <FormErrorMessage>{errors.username && errors.username.message}</FormErrorMessage>
-            <FormLabel htmlFor='password'>Password</FormLabel>
-            <Input id='password' placeholder='Password' {...register('password', {
-              required: 'Password is required'
-            })} />
-            <Button colorScheme="teal" type='submit'>Login</Button>
+          <FormControl isInvalid={errors.username}>
+            <FormLabel htmlFor="username">Username</FormLabel>
+            <Input
+              id="username"
+              placeholder="Username"
+              {...register("username", {
+                required: "Username is required",
+              })}
+            />
+            <FormErrorMessage>
+              {errors.username && errors.username.message}
+            </FormErrorMessage>
           </FormControl>
-          </form>
+          <FormControl isInvalid={errors.username}>
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              id="password"
+              placeholder="Password"
+              {...register("password", {
+                required: "Password is required",
+              })}
+            />
+            <FormErrorMessage>
+              {errors.password && errors.password.message}
+            </FormErrorMessage>
+            <Button colorScheme="teal" type="submit">
+              Login
+            </Button>
+          </FormControl>
+        </form>
       </Container>
     </VStack>
   );
